d1362da15b9c016c48a2fdbd94d1ef87fed4c173
nobre14 <rafanobre14@hotmail.com>
2018-05-16 14:50:30 -0300

    Inserido o consign e definida variavel que contem o modulo de conexao com o DB

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,10 +1,12 @@
-var dbConnection = require('../../config/dbConnection');
+//var dbConnection = require('../../config/dbConnection');
 
 module.exports = function(app){
-	var connection = dbConnection();
+	
 	app.get('/noticias', function(req, res){
+		var connection = app.config.dbConnection();// recupera o modulo que faz conexao com o banco
 		connection.query('select * from noticias', function(error, result){
 			res.render("noticias/noticias", {noticias : result});
 		});
 	});
-}
+	
+};
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,11 +1,16 @@
 var mysql = require('mysql');
 		
-module.exports = function(){
+var conMySQL = function(){
+	console.log("Conexao com db foi estabelecida");
 	return mysql.createConnection({
 			host:'localhost',
 			user:'root',
 			password:'',
 			database:'portal_noticias'
-		});
+	});
+}
+module.exports = function(){
+	console.log("Autoload carregou modulo de conexao com o bd")
+	return conMySQL;
 }
 		
